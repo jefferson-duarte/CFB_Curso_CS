81287788f1587301ca0eecda6e01a305b17d550a
Jefferson Duarte <jeffersonsduarte@live.com>
2023-09-26 10:09:04 +0100

    aula sobre variaveis const

---
+++ Aula07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Aula07
+{
+    static void Main()
+    {
+        // a variavel const nao pode ser alterado
+        const string canal = "CFB Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal: {0}\nPi: {1}", canal, pi);
+    }
+}
