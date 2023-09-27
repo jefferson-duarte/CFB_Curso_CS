30cd3612d0acd2625b892faee0c2153be41c7820
Jefferson Duarte <jeffersonsduarte@live.com>
2023-09-27 22:33:10 +0100

    utilizando o metodo enum

---
+++ Aula10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Aula10
+{
+    enum DiasSemana { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };
+    static void Main()
+    {
+        // usando casting mostra o indice da variavel
+        int ds2 = (int)DiasSemana.Quinta;
+
+        DiasSemana ds = DiasSemana.Domingo;
+
+        // usando casting e pegando o valor pelo indice 3
+        DiasSemana ds1 = (DiasSemana)3;
+        
+        Console.WriteLine(ds);
+        Console.WriteLine(ds1);
+        Console.WriteLine(ds2);
+    }
+}
