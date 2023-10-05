1e9e4c98ebdf5846db9095ed28fc38fbd64f6c39
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-05 09:07:33 +0100

    aula sobre if alinhado

---
+++ Aula14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Aula14
+{
+    static void Main()
+    {
+        float n1, n2, n3, media;
+        string resultado;
+
+        Console.Write("Digite a nota 1: ");
+        n1 = float.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 2: ");
+        n2 = float.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 3: ");
+        n3 = float.Parse(Console.ReadLine());
+
+        media = (n1 + n2 + n3) / 3;
+
+        if (media >= 60)
+        {
+            if (media >= 90)
+            {
+                resultado = "Aprovado com louvor";
+            }
+            else
+            {
+                resultado = "Aprovado";
+            }
+        } 
+        else
+        {
+            if (media >= 40)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+        }
+
+        Console.WriteLine($"Sua media entre {n1} + {n2} + {n3} sua média é: {media}");
+        Console.WriteLine($"Seu resulado é: {resultado}");
+    }
+}
