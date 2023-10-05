922fd2a6bf022a66b5f890950f8726ee3e0b22fd
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-05 09:31:55 +0100

    aula sobre switch

---
+++ Aula15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Aula15
+{
+    static void Main()
+    {
+        int tempo;
+        char escolha;
+        string tipoTransporte = " ";
+
+        Console.WriteLine("Belo Horizonte/MG a Vitória/ES");
+        Console.WriteLine("Escolha o transporte:");
+        Console.WriteLine("[A] Avião");
+        Console.WriteLine("[C] Carro");
+        Console.WriteLine("[O] Ônibus");
+
+
+        escolha = char.Parse(Console.ReadLine().ToUpper());
+
+        switch(escolha)
+        {
+            case 'A':
+                tempo = 50;
+                tipoTransporte = "Avião";
+                break;
+            case 'C':
+                tempo = 480;
+                tipoTransporte = "Carro";
+                break;
+            case 'O':
+                tempo = 660;
+                tipoTransporte = "Ônibus";
+                break;
+            default:
+                tempo = -1;
+                break;
+
+        }
+
+        if (tempo < 0) 
+        {
+            Console.WriteLine("Transporte Indisponível");
+        }
+        else
+        {
+            Console.WriteLine($"O transporte escolhido foi {tipoTransporte} e o tempo é de {tempo} minutos.");
+        }
+    }
+}
