56c08baf6a204b84e36558ea5e1c670851455f9c
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-10 10:27:28 +0100

    Aula loop for

---
+++ Aula19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Aula19
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+
+        for(int i=0; i<num.Length; i++)
+        {
+            num[i] = 0;
+            Console.WriteLine($"Valor de num na pos {i}: {num[i]}");
+        }
+
+        Console.WriteLine();
+
+        for(int i=1; i <= num.Length; i++)
+        {
+            Console.WriteLine($"{i} - Lista.");
+        }
+    }
+}
