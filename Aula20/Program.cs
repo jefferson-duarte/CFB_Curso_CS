758fbdf681249558e52a5c819f665832fec2c2c2
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-10 10:52:01 +0100

    comando while

---
+++ Aula20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Aula20
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+        int i = 0, j = 0;
+
+        while (i < num.Length)
+        {
+            Console.WriteLine(num[i]);
+            i++;
+        }
+
+        Console.WriteLine();
+
+        while (j < num.Length)
+        {
+            Console.WriteLine($"Valor - {j}");
+            j++;
+        }
+        Console.WriteLine("Fim do while");
+    }
+}
