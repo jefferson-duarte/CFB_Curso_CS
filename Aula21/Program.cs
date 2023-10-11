0b4a4670ca61dd8ebf663fe7e1811a97e78f52fc
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-11 23:08:03 +0100

    Aula comando Do While

---
+++ Aula21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Aula21
+{
+    static void Main()
+    {
+
+        string senha = "123";
+        string senhaUser;
+        int tentativas = 0;
+
+        do
+        {
+            Console.Clear();
+            Console.Write("Digite sua senha: ");
+            senhaUser = Console.ReadLine();
+            tentativas++;
+
+        } while (senha != senhaUser);
+
+        Console.WriteLine($"Senha correta, tentativas: {tentativas}");
+    }
+}
