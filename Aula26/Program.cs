ca3a85f6f892ce6c97b837800baefcd881ba0411
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-17 10:13:44 +0100

    usando argumento out nos metodos

---
+++ Aula26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Aula26
+{
+    static void Main()
+    {
+        float numDividir, numDivisor, resultDivisao, restoDivisao;
+
+        Console.WriteLine("Divisao");
+
+        Console.Write("Digite o numero que quer dividir: ");
+        numDividir = float.Parse(Console.ReadLine());
+        Console.Write("Digite por quanto quer dividir: ");
+        numDivisor = float.Parse(Console.ReadLine());
+
+        resultDivisao = divide(numDividir, numDivisor, out restoDivisao);
+        Console.WriteLine($"A divisao entre {numDividir} e {numDivisor} = {resultDivisao}");
+        Console.WriteLine($"O resto da divisao e {restoDivisao}");
+    }
+
+    static float divide(float dividendo, float divisor, out float resto)
+    {
+        float quociente;
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+        return quociente;
+    }
+}
