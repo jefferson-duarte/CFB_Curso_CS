327587e01618958229440cb0162222413f144ced
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-17 10:32:42 +0100

    inserindo mais de um argumento no metodo

---
+++ Aula27/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Aula27
+{
+    static void Main()
+    {
+        //Console.WriteLine("SOMA");
+        float valores;
+        valores = soma(1, 4, 5, 6);
+        Console.WriteLine($"O retorno foi: {valores}");
+    }
+
+    static float soma(params float[] n)
+    {
+        float res = 0;
+
+        if (n.Length < 1)
+        {
+            Console.WriteLine("Nao a valores para somar.");
+        }else if(n.Length < 2)
+        {
+            Console.WriteLine($"Valores insuficientes para somar {n[0]}");
+        }
+        else
+        {
+            for(int i = 0; i < n.Length; i++)
+            {
+                res += n[i];
+            }
+        }
+
+        return res;
+    }
+}
