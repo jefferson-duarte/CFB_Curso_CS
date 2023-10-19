b2528fe010b0e986f0f1ef7b3bde68b42d8cc1ee
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-19 23:32:05 +0100

    exemplo da ordem de execução dos construtores

---
+++ Aula37/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Base
+{
+    public Base() {
+        Console.WriteLine("Construtor da Base");
+    }
+}
+
+class Derivada1 : Base
+{
+    public Derivada1()
+    {
+        Console.WriteLine("Construtor da Derivada 1");
+    }
+}
+
+class Derivada2 : Derivada1
+{
+    public Derivada2()
+    {
+        Console.WriteLine("Construtor da Derivada 2");
+    }
+}
+
+class Aula37
+{
+    static void Main() 
+    {
+        var derivada2 = new Derivada2();
+    }
+}
