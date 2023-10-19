157e3940d66214ebbcb7e82a62a544c73fee79f4
Jefferson Duarte <jeffersonsduarte@live.com>
2023-10-19 23:11:20 +0100

    aula sobre membros protected

---
+++ Aula36/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Veiculo
+{
+    public int velAtual;
+    private int velMax;
+    protected bool ligado;
+
+    public Veiculo(int velMax) 
+    {
+        velAtual = 0;
+        this.velMax = velMax;
+        ligado = false;
+    }
+
+    public string getLigado() 
+    {
+        return (ligado?"Sim":"Não");
+    }
+
+    public int getVelMax() 
+    { 
+        return velMax;
+    }
+
+}
+
+class Carro : Veiculo
+{
+    public string nome;
+
+    public Carro(string nome, int vm):base(vm) //vm = velocidade maxima
+    {
+        this.nome = nome;
+        ligado = false;
+    }
+}
+
+class Aula36
+{    
+    static void Main()
+    {
+        var c1 = new Carro("Golf", 120);       
+
+        Console.WriteLine("----------------------");
+        Console.WriteLine($"Nome............: {c1.nome}");
+        Console.WriteLine($"Velocidade Atual: {c1.velAtual}");
+        Console.WriteLine($"Velocidade Max..: {c1.getVelMax()}");
+        Console.WriteLine($"Ligado..........: {c1.getLigado()}");
+        Console.WriteLine("----------------------");
+    }
+}
